cdf2ad82cba6592ca6238b3d640cc58e7cda9a10
Kaushik Donthi <kaushikdr@gmail.com>
2022-11-05 19:54:28 -0700

    Fixed tests and brought back deleted functions

--- backend/testing/common.go
+++ backend/testing/common.go
@@ -3,6 +3,8 @@ package testing
 import (
 	"TodoApplication/backend"
 	"TodoApplication/utils"
+	"bytes"
+	"encoding/json"
 	"github.com/julienschmidt/httprouter"
 )
 
@@ -11,4 +13,18 @@ func setup() (*httprouter.Router, *utils.ItemList) {
 	itemList := utils.NewItemList()
 	backend.SetHandlers(router, itemList)
 	return router, itemList
+}
+
+func createValidRequestBody(item string) *bytes.Buffer {
+	b, _ := json.Marshal(backend.RequestBody{Item: item})
+	return bytes.NewBuffer(b)
+}
+
+type invalidRequestBody struct {
+	Noop string `json:"noop"`
+}
+
+func createInvalidRequestBody() *bytes.Buffer {
+	b, _ := json.Marshal(invalidRequestBody{Noop: "123"})
+	return bytes.NewBuffer(b)
 }
